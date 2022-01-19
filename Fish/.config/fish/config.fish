5b1a24f54af0f89fecf29e431775c403114f168f
Faiz Saiyad <fsaiyad990@gmail.com>
2022-01-19 22:31:06 +0530

    Adding submodule

--- Fish/.config/fish/config.fish
+++ Fish/.config/fish/config.fish
@@ -1,7 +1,6 @@
 starship init fish | source
 set -gx LIBGL_ALWAYS_INDIRECT 1 #GWSL
 
-
 set -x PATH "/mnt/e/Development/flutter/bin" $PATH
 set -x PATH "/home/reaperfs/.cargo/bin" $PATH
 set -x PATH "/home/reaperfs/.local/bin" $PATH
@@ -16,3 +15,4 @@ alias v "nvim"
 lua ~/.config/fish/conf.d/z.lua/z.lua --init fish | source
 
 eval "tmux attach -t base || tmux new -s base"
+
